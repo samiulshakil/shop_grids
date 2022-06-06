dc460d8361b80299c5e4b8c06470fabf756b3749
samiulshakil <samiulshakil@gmail.com>
2022-06-07 03:11:48 +0600

    featured categories dynamic frontend

--- app/Http/Controllers/Frontend/WebsiteController.php
+++ app/Http/Controllers/Frontend/WebsiteController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Banner;
+use App\Models\Category;
 
 class WebsiteController extends Controller
 {
--- resources/views/frontend/include/featured_category.blade.php
+++ resources/views/frontend/include/featured_category.blade.php
@@ -10,108 +10,25 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4 col-md-6 col-12">
+            @forelse (categories() as $category)
+                <div class="col-lg-4 col-md-6 col-12">
 
-                <div class="single-category">
-                    <h3 class="heading">TV & Audios</h3>
-                    <ul>
-                        <li><a href="product-grids.html">Smart Television</a></li>
-                        <li><a href="product-grids.html">QLED TV</a></li>
-                        <li><a href="product-grids.html">Audios</a></li>
-                        <li><a href="product-grids.html">Headphones</a></li>
-                        <li><a href="product-grids.html">View All</a></li>
-                    </ul>
-                    <div class="images">
-                        <img src="{{asset('frontend/assets/images/featured-categories/fetured-item-1.png')}}" alt="#">
+                    <div class="single-category">
+                        <h3 class="heading">{{ $category->category_name }}</h3>
+                        <ul>
+                            @forelse ($category->subCategories as $subcategory)
+                                <li><a href="product-grids.html">{{ $subcategory->sub_category_name }}</a></li>
+                            @empty
+                            @endforelse
+                        </ul>
+                        <div class="images">
+                            <img src="{{ asset($category->category_image) }}" alt="#">
+                        </div>
                     </div>
+
                 </div>
-
-            </div>
-            <div class="col-lg-4 col-md-6 col-12">
-
-                <div class="single-category">
-                    <h3 class="heading">Desktop & Laptop</h3>
-                    <ul>
-                        <li><a href="product-grids.html">Smart Television</a></li>
-                        <li><a href="product-grids.html">QLED TV</a></li>
-                        <li><a href="product-grids.html">Audios</a></li>
-                        <li><a href="product-grids.html">Headphones</a></li>
-                        <li><a href="product-grids.html">View All</a></li>
-                    </ul>
-                    <div class="images">
-                        <img src="{{asset('frontend/assets/images/featured-categories/fetured-item-2.png')}}" alt="#">
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-lg-4 col-md-6 col-12">
-
-                <div class="single-category">
-                    <h3 class="heading">Cctv Camera</h3>
-                    <ul>
-                        <li><a href="product-grids.html">Smart Television</a></li>
-                        <li><a href="product-grids.html">QLED TV</a></li>
-                        <li><a href="product-grids.html">Audios</a></li>
-                        <li><a href="product-grids.html">Headphones</a></li>
-                        <li><a href="product-grids.html">View All</a></li>
-                    </ul>
-                    <div class="images">
-                        <img src="{{asset('frontend/assets/images/featured-categories/fetured-item-3.png')}}" alt="#">
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-lg-4 col-md-6 col-12">
-
-                <div class="single-category">
-                    <h3 class="heading">Dslr Camera</h3>
-                    <ul>
-                        <li><a href="product-grids.html">Smart Television</a></li>
-                        <li><a href="product-grids.html">QLED TV</a></li>
-                        <li><a href="product-grids.html">Audios</a></li>
-                        <li><a href="product-grids.html">Headphones</a></li>
-                        <li><a href="product-grids.html">View All</a></li>
-                    </ul>
-                    <div class="images">
-                        <img src="{{asset('frontend/assets/images/featured-categories/fetured-item-4.png')}}" alt="#">
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-lg-4 col-md-6 col-12">
-
-                <div class="single-category">
-                    <h3 class="heading">Smart Phones</h3>
-                    <ul>
-                        <li><a href="product-grids.html">Smart Television</a></li>
-                        <li><a href="product-grids.html">QLED TV</a></li>
-                        <li><a href="product-grids.html">Audios</a></li>
-                        <li><a href="product-grids.html">Headphones</a></li>
-                        <li><a href="product-grids.html">View All</a></li>
-                    </ul>
-                    <div class="images">
-                        <img src="{{asset('frontend/assets/images/featured-categories/fetured-item-5.png')}}" alt="#">
-                    </div>
-                </div>
-
-            </div>
-            <div class="col-lg-4 col-md-6 col-12">
-
-                <div class="single-category">
-                    <h3 class="heading">Game Console</h3>
-                    <ul>
-                        <li><a href="product-grids.html">Smart Television</a></li>
-                        <li><a href="product-grids.html">QLED TV</a></li>
-                        <li><a href="product-grids.html">Audios</a></li>
-                        <li><a href="product-grids.html">Headphones</a></li>
-                        <li><a href="product-grids.html">View All</a></li>
-                    </ul>
-                    <div class="images">
-                        <img src="{{asset('frontend/assets/images/featured-categories/fetured-item-6.png')}}" alt="#">
-                    </div>
-                </div>
-
-            </div>
+            @empty
+            @endforelse
         </div>
     </div>
 </section>
